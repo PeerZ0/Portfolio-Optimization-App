6a136aed8f3a73feed4b1652a43b56b920d62c02
PeerZ0 <122899247+PeerZ0@users.noreply.github.com>
2024-11-22 16:13:25 +0100

    updates

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from models.user import User
-from services import *
+from services.gather_information import gather_information
+from services.update_data import update_data
 
 def main():
     user = User()
+    update_data()
     gather_information(user)
     user.optimize_portfolio()
     
